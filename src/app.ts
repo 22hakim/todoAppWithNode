c4029fc28ed18bc6eefb23cbf325cf0ee1844862
hakim <hakim_yahi@hotmail.fr>
2023-11-25 18:33:54 +0100

    Add config

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,16 @@ import express, { Express } from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import todoRoutes from "./routes"
+const config = require("../config/config.dev.json");
 
 const app: Express = express()
 
-const PORT: string | number = process.env.PORT || 4000
+const PORT: string | number = config.PORT || 4000
 
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = process.env.MONGO_URL || "";
+const uri: string = config.MONGO_URL || "";
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 
 mongoose.connect(uri)

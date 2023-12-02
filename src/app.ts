310674a8c6494a3c3eb4624c57be51d2ab8cb40f
hakim <hakim_yahi@hotmail.fr>
2023-12-02 19:15:45 +0100

    Make request working

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,27 @@ import express, { Express } from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import todoRoutes from "./routes"
+import bodyParser from "body-parser"
+
 const config = require("../config/config.dev.json");
-
 const app: Express = express()
+const PORT: string | number = config.PORT
+const uri: string = config.MONGO_URL || "";
 
-const PORT: string | number = config.PORT || 4000
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = config.MONGO_URL || "";
-const options = { useNewUrlParser: true, useUnifiedTopology: true }
+console.log(uri);
 
 mongoose.connect(uri)
-        .then(()=>{console.log(`Server running on http://localhost:${PORT}`)})
-        .catch(error => {throw error})
+mongoose.connection.once('open',() => { 
+    app.emit('ready'); 
+});
+
+app.on('ready', function() { 
+    app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`); 
+  });
+});  
